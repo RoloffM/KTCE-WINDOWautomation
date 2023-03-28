5125076f49889ac5f9dbf8e5f85694d55ccc467e
RoloffM <taghavi.sina@gmail.com>
2023-03-28 19:40:01 +0200

    init project initializer
    spring boot init

---
+++ WindowAutomation/WindowAutomation/src/main/java/at/jku/ktce/WindowAutomation/WindowAutomationApplication.java
@@ -0,0 +1,13 @@
+package at.jku.ktce.WindowAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WindowAutomationApplication.class, args);
+	}
+
+}
---
+++ WindowAutomation/WindowAutomation/src/test/java/at/jku/ktce/WindowAutomation/WindowAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package at.jku.ktce.WindowAutomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
